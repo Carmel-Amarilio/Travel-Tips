4692eaff57cbcee056607dc1e21828e17b7628ff
AlmogJan <Almogj1998@gmail.com>
2023-08-30 22:37:39 +0300

    css

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -6,7 +6,7 @@ export const mapService = {
   panTo,
   removePlace,
   searchLocation,
-  addPlace
+  addPlace,
 };
 
 // Var that is used throughout this Module (not global)
@@ -18,48 +18,49 @@ let gMarkers = [];
 
 function initMap(lat = 51.505591765464914, lng = -0.09048158007556373) {
   console.log("InitMap");
-  getWeather(lat, lng).then(res => rentWeather(res))
+  getWeather(lat, lng).then((res) => rentWeather(res));
   return _connectGoogleApi().then(() => {
     console.log("google available");
     gMap = new google.maps.Map(document.querySelector("#map"), {
       center: { lat, lng },
       zoom: 15,
     });
-    gMap.addListener('click', ev => {
-      const name = prompt('Place name?', 'Place 1')
-      if (!name) return
-      const lat = ev.latLng.lat()
-      const lng = ev.latLng.lng()
-      addPlace(name, lat, lng, gMap.getZoom())
-      getWeather(lat, lng).then(res => rentWeather(res))
-    })
+    gMap.addListener("click", (ev) => {
+      const name = prompt("Place name?", "Place 1");
+      if (!name) return;
+      const lat = ev.latLng.lat();
+      const lng = ev.latLng.lng();
+      addPlace(name, lat, lng, gMap.getZoom());
+      getWeather(lat, lng).then((res) => rentWeather(res));
+    });
     console.log("Map!", gMap);
   });
-  
 }
 
 function getWeather(lat, lng) {
-  return fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit5&appid=307b1a8173ac11cf753e139fe19fa56e`)
-    .then(res => res.json())
-    .then(lok => {
-      const city = lok[0].name
-      const country = lok[0].country
-      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=307b1a8173ac11cf753e139fe19fa56e`)
-        .then(res => res.json())
-        .then(weather => {
+  return fetch(
+    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&limit5&appid=307b1a8173ac11cf753e139fe19fa56e`
+  )
+    .then((res) => res.json())
+    .then((lok) => {
+      const city = lok[0].name;
+      const country = lok[0].country;
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=307b1a8173ac11cf753e139fe19fa56e`
+      )
+        .then((res) => res.json())
+        .then((weather) => {
           return {
-            city, 
+            city,
             country,
             temp: weather.main.temp,
             tempMin: weather.main.temp_min,
             tempMax: weather.main.temp_max,
             weather: weather.weather[0].main,
             wind: weather.wind.speed,
-          }
-        })
-    })
-
-    
+          };
+        });
+    });
 }
 
 function addMarker(loc) {
@@ -74,16 +75,17 @@ function addMarker(loc) {
 function panTo(lat, lng) {
   var laLatLng = new google.maps.LatLng(lat, lng);
   gMap.panTo(laLatLng);
-  getWeather(lat, lng).then(res => rentWeather(res))
+  getWeather(lat, lng).then((res) => rentWeather(res));
 }
 
 function removePlace(placeId) {
   const deleteIndex = gPlaces.findIndex((Place) => Place.id === placeId);
   gPlaces.splice(deleteIndex, 1);
   // saveToStorage('placesDB', gPlaces)
-  renderPlaces(gPlaces)
-  renderMarkers(gPlaces)
-  if(gPlaces[0])getWeather(gPlaces[0].lat, gPlaces[0].lng).then(res => rentWeather(res))
+  renderPlaces(gPlaces);
+  renderMarkers(gPlaces);
+  if (gPlaces[0])
+    getWeather(gPlaces[0].lat, gPlaces[0].lng).then((res) => rentWeather(res));
 }
 
 function _connectGoogleApi() {
@@ -114,18 +116,18 @@ function _createPlace(name, lat, lng, zoom) {
     lat,
     lng,
     zoom,
-  }
+  };
 }
 
 function renderPlaces(places) {
   const placesHtml = places
     .map(
       (place) => `
-      <li>
-          <span>${place.name}:</span>
+      <p>
+          <span>${place.name}</span>
           <button onclick="onPanTo(${place.lat}, ${place.lng})"class="btn-pan btn"><i class="fa-solid fa-location-dot"></i></button>
           <button onclick="OnRemove('${place.id}')" class="btn"><i class="fa-solid fa-trash"></i></button>
-      </li>
+      </p>
   `
     )
     .join("");
@@ -133,24 +135,25 @@ function renderPlaces(places) {
 }
 
 function renderMarkers(places) {
-  if (gMarkers) gMarkers.forEach(marker => marker.setMap(null))
-  gMarkers = places.map(place => {
+  if (gMarkers) gMarkers.forEach((marker) => marker.setMap(null));
+  gMarkers = places.map((place) => {
     return new google.maps.Marker({
       position: place,
       map: gMap,
-      title: place.name
-    })
-  })
+      title: place.name,
+    });
+  });
 }
 
-function rentWeather({city, country, temp, tempMin, tempMax, weather, wind}){
-  document.querySelector('.weather').innerHTML = `
-  <h3>weather today</h3>
+function rentWeather({ city, country, temp, tempMin, tempMax, weather, wind }) {
+  document.querySelector(".weather").innerHTML = `
+  <h3 class="secondary">Weather Today</h3>
   <p>${city}, ${country} <span>${weather}</span></p>
-  <p>${Math.trunc(temp- 273.15)}C temperature from ${Math.trunc(tempMin- 273.15)} to ${Math.trunc(tempMax- 273.15)}C, wind ${wind}m/s</p>
-  `
-  document.querySelector('.user-pos').innerHTML =` ${city},  ${country}`
-  
+  <p>${Math.trunc(temp - 273.15)}C temperature from ${Math.trunc(
+    tempMin - 273.15
+  )} to ${Math.trunc(tempMax - 273.15)}C, wind ${wind}m/s</p>
+  `;
+  document.querySelector(".user-pos").innerHTML = ` ${city},  ${country}`;
 }
 
 function searchLocation(value) {
